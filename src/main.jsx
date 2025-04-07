269c129c79d6de52610be8241fa93070611fbd1e
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-07 16:09:05 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+import './index.css'
+import App from './App.jsx'
+import Contact from './contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
